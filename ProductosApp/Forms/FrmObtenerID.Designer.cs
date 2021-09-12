31598ad08bc84a8e9b313682b000a66db6df66c9
josepejunior <josephmenjunior@gmail.com>
2021-09-11 18:38:35 -0600

    changes

---
+++ ProductosApp/Forms/FrmObtenerID.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace ProductosApp.Forms
+{
+    partial class FrmObtenerID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmObtenerID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 172);
+            this.Name = "FrmObtenerID";
+            this.Text = "FrmObtenerID";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
